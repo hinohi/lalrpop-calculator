21edcb1c7cd37a8528acc55d23bc077e06079721
Nakayama Daiju <42.daiju@gmail.com>
2020-09-22 19:20:20 +0900

    add +-*/

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn calculator1() {
-        assert!(grammar::TermParser::new().parse("22").is_ok());
-        assert!(grammar::TermParser::new().parse("(22)").is_ok());
-        assert!(grammar::TermParser::new().parse("((((22))))").is_ok());
-        assert!(grammar::TermParser::new().parse("((22)").is_err());
+    fn term() {
+        assert_eq!(grammar::ExprParser::new().parse("22").unwrap(), 22);
+        assert_eq!(grammar::ExprParser::new().parse("(23)").unwrap(), 23);
+        assert_eq!(grammar::ExprParser::new().parse("((((24))))").unwrap(), 24);
+        assert!(grammar::ExprParser::new().parse("((22)").is_err());
+    }
+
+    #[test]
+    fn expr() {
+        assert_eq!(grammar::ExprParser::new().parse("2 + 3 * 4"), Ok(14));
+        assert_eq!(grammar::ExprParser::new().parse("6 / 2 * 3"), Ok(9));
     }
 }
